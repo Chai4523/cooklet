9e6a081ea6f2c216a2e03ca189ff9a70b829bad9
Chai Chung Yee <chungyee@fomatictech.com>
2025-02-24 11:08:16 +0800

    setup firebase

--- src/APIUtils.js
+++ src/APIUtils.js
@@ -1,5 +1,17 @@
-const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY
+import { initializeApp } from "firebase/app";
 
+const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY
+const firebaseConfig = {
+    apiKey: "***REMOVED***",
+    authDomain: "recipe-app-63bae.firebaseapp.com",
+    projectId: "recipe-app-63bae",
+    storageBucket: "recipe-app-63bae.firebasestorage.app",
+    messagingSenderId: "222113831421",
+    appId: "1:222113831421:web:468df3a10318b28bf22194",
+    measurementId: "G-W33LRL03Z5"
+  };
+
+  const app = initializeApp(firebaseConfig);
 export function searchRecipe(query = "pasta", number = 10) {
     const url = new URL("https://api.spoonacular.com/recipes/complexSearch")
     const queryParams = {
