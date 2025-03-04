226a8665189bcad56900c03ad5df4c3f242dc4c5
Chai Chung Yee <chaichungyee@gmail.com>
2025-03-04 16:20:27 +0800

    added some css (web view)

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,52 @@ import * as api from "./APIUtils";
 function App() {
   return (
     <div>
-      <h1>Recipe Search</h1>
-      <h2>Search Recipes from all over the world</h2>
+      <h1 className="title">Recipe Search</h1>
+      <h2 className="subtitle">Search recipes from all over the world.</h2>
 
       <form
+        className="form"
         onSubmit={(e) => {
           e.preventDefault();
           e.stopPropagation();
         }}
       >
-        <input type="text" placeholder="What would you like to cook?" />
+        <input
+          className="input-box text-base semibold"
+          type="text"
+          placeholder="Enter a recipe"
+        />
 
-        <div>
-          <label>Diet</label>
-          <select>
-            {[
-              "none",
-              "pescetarian",
-              "lacto vegetarian",
-              "ovo vegetarian",
-              "vegan",
-              "vegetarian",
-            ].map((diet) => {
-              return <option value={diet}>{diet}</option>;
-            })}
-          </select>
+        <div className="flex-row">
+          <div className="flex-col flex-grow mw-one-third">
+            <label>Diet</label>
+            <select className="input-box text-base semibold mt-1">
+              {[
+                "none",
+                "pescetarian",
+                "lacto vegetarian",
+                "ovo vegetarian",
+                "vegan",
+                "vegetarian",
+              ].map((diet) => {
+                return <option value={diet}>{diet}</option>;
+              })}
+            </select>
+          </div>
+
+          <div className="flex-col flex-grow mw-one-third">
+            <label>Exclude Ingredients</label>
+            <input
+              className="input-box text-base semibold mt-1"
+              type="text"
+              placeholder="Allergic to something? Type here"
+            />
+          </div>
         </div>
 
-        <div>
-          <label>Exclude Ingredients</label>
-          <input type="text" placeholder="Allergic to something? Type here"/>
-        </div>
-
-        <button type="submit">Search</button>
+        <button className="btn-search w-full text-base bold" type="submit">
+          Search
+        </button>
       </form>
     </div>
   );
